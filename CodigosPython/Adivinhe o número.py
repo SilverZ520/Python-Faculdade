d7cb1626c88ef67e885e79e23dd0cda8751cd91a
SilverZ520 <114871864+SilverZ520@users.noreply.github.com>
2026-05-22 23:56:52 -0300

    Adicionado Adivinhe o número.py

---
+++ CodigosPython/Adivinhe o número.py
@@ -0,0 +1,34 @@
+# Jogo simples de adivinhar o número aleatório
+
+import random
+
+menor_numero = 1
+maior_numero = 50
+resposta = random.randint(menor_numero, maior_numero)
+tentativas = 0
+esta_correndo = True
+
+print("Jogo de adivinhar o número!")
+print(f"Tente adivinhar o número que está entre {menor_numero} e {maior_numero}!")
+
+while esta_correndo:
+    chute = (input("Adivinhe: "))
+
+    if chute.isdigit():
+        chute = int(chute)
+        tentativas += 1
+
+        if chute < menor_numero or chute > maior_numero:
+            print("Uh, acho que foi longe demais, não?")
+            print(f"Por favor, tente outro número entre {menor_numero} e {maior_numero}!")
+        elif chute < resposta:
+            print("Uff, baixo! Tente de novo!")
+        elif chute > resposta:
+            print("Uff, alto! Tente de novo!")
+        else:
+            print(f"CORRETO! A resposta era {resposta}")
+            print(f"Número de tentativas: {tentativas}")
+            esta_correndo = False
+    else:
+        print("Inválido.")
+        print(f"Por favor, tente outro número entre {menor_numero} e {maior_numero}!")
